d23695bae5296c0ca4c66f81a920a9b120bff6c5
Your User Name <dicksonmwadime2020@gmail.com>
2023-04-03 01:49:51 +0300

    object commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee ={
+    name:'Sam',
+    streetAddress:'gachoe'
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newEmployee={...employee}
+    return newEmployee.key=value
+}
+
+updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway')
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    return employee.key=value
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway')
+
+function deleteFromEmployeeByKey(employee, key){
+    const {[key]:_, ...newEmployee} =  employee
+    return newEmployee
+}
+
+deleteFromEmployeeByKey(employee, 'name')
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
+destructivelyDeleteFromEmployeeByKey(employee, 'name')
+
